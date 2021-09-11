39d6a31cd90b5b3b3a4189e81f9f8705681278d2
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-10 21:11:11 -0500

    [sm] added put method in API along with a put endpoint

--- cors/rest/server.go
+++ cors/rest/server.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	// "strconv"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,9 +25,21 @@ func GetMessages(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"messages": messages})
 }
 
+func PutMessage(c *gin.Context) {
+	version := c.Param("version")
+	id, _ := strconv.Atoi(c.Param("id"))
+	text := c.PostForm("puttext")
+	messages[id] = text
+	if version == "v2" {
+	    c.Header("Access-Control-Allow-Origin", "http://localhost:8080")
+	}
+	c.JSON(http.StatusOK, gin.H{"messages": messages})
+}
+
 func main() {
 	messages = append(messages, "Hello CORS!")
 	r := gin.Default()
 	r.GET("/api/:version/messages", GetMessages)
+	r.PUT("/api/:version/messages/:id", PutMessage)
 	r.Run(":8000")
 }
