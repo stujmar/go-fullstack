088ff9ea5af5414ef30f9afcd3a0615c677a8744
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-09-13 22:22:00 -0500

    [sm] added some code in the control.js to add content to the headers area on put requests

--- cors/frontend/control.js
+++ cors/frontend/control.js
@@ -21,6 +21,31 @@ function onGet(version) {
     });
 }
 
+// function onPut(version) {
+//     const url = "http://localhost:8000/api/" + version + "/messages/0";
+//     // var headers = {}
+//     let headers = { "X-Token": "abcd1234" }
+//     console.log("frontend PUTTING:", version);
+//     fetch(url, {
+//         method : "PUT",
+//         mode: 'cors',
+//         headers: headers,
+//         body: new URLSearchParams(new FormData(document.getElementById("form2"))),
+//     })
+//     .then((response) => {
+//         if (!response.ok) {
+//             throw new Error(response.error)
+//         }
+//         return response.json();
+//     })
+//     .then(data => {
+//         document.getElementById('messages').value = data.messages;
+//     })
+//     .catch(function(error) {
+//         document.getElementById('messages').value = error;
+//     });
+// }
+
 function onPut(version) {
     const url = "http://localhost:8000/api/" + version + "/messages/0";
     // var headers = {}
@@ -36,12 +61,9 @@ function onPut(version) {
         if (!response.ok) {
             throw new Error(response.error)
         }
+        response.headers.forEach(function(val, key) {
+            document.getElementById('headers').value += '\n' + key + ': ' + val; 
+        });
         return response.json();
     })
-    .then(data => {
-        document.getElementById('messages').value = data.messages;
-    })
-    .catch(function(error) {
-        document.getElementById('messages').value = error;
-    });
 }
